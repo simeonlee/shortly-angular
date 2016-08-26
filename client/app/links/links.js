9f90e9aaf548098bb1be2fa8b77059b2a394d6ca
simeonlee <simeonhlee@gmail.com>
2016-08-26 15:32:55 -0700

    Pass All Tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,11 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  // console.log(Links.getAll());
+  // $scope.data.links = Links.getAll();
+  Links.getAll()
+    .then(function(links) {
+      $scope.data.links = links;
+    });
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,32 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getAll = function () {
+    return $http({
+      method: 'GET',
+      url: '/api/links',
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
+  };
+
+  var addOne = function (link) {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: link
+    })
+    .then(function (resp) {
+      // console.log('resp is:', resp);
+      return resp;
+    });
+  };
+
+  return {
+    addOne: addOne,
+    getAll: getAll
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
